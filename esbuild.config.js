1ad59646f6d01f36244e8f94cea096cceeedee4c
Alex Contreras <alexis.contreras@mercadolibre.com>
2022-01-27 19:08:57 -0300

    cli init

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,71 +1,72 @@
-import chokidar from "chokidar";
+import chokidar from 'chokidar';
 import esbuild from 'esbuild';
 /* import sassEs from "essass"; */
-import serve, { error, log } from 'create-serve';
-import sass from "sass";
-import path from "path";
+import serve from 'create-serve';
+import sass from 'sass';
+import path from 'path';
 
 let SERVER_IS_RUNNING = false;
 
 const sassPlugin = ({ verbose = false } = {}) => {
-  return {
-    name: "sass",
-    setup(build) {
-      build.onResolve({ filter: /\.scss$/ }, (args) => {
-        if (verbose) {
-          console.log(args.resolveDir);
-        }
-        let paths = {
-          path: path.resolve(args.resolveDir, args.path),
-          resPath: args.resolveDir
-        }
-        return {
-          path: JSON.stringify(paths),
-          namespace: "sass",
-        };
-      });
-      build.onLoad({ filter: /.*/, namespace: "sass" }, (args) => {
-        let { path, resPath } = JSON.parse(args.path)
-        let compiled = sass.renderSync({ file: path });
-        if (verbose) {
-          console.log(args);
-        }
-        return {
-          resolveDir: resPath,
-          contents: compiled.css.toString(),
-          loader: "css",
-        };
-      });
-    },
-  };
-}
+	return {
+		name: 'sass',
+		setup(build) {
+			build.onResolve({ filter: /\.scss$/ }, (args) => {
+				if (verbose) {
+					console.log(args.resolveDir);
+				}
+				let paths = {
+					path: path.resolve(args.resolveDir, args.path),
+					resPath: args.resolveDir
+				};
+				return {
+					path: JSON.stringify(paths),
+					namespace: 'sass',
+				};
+			});
+			build.onLoad({ filter: /.*/, namespace: 'sass' }, (args) => {
+				let { path, resPath } = JSON.parse(args.path);
+				let compiled = sass.renderSync({ file: path });
+				if (verbose) {
+					console.log(args);
+				}
+				return {
+					resolveDir: resPath,
+					contents: compiled.css.toString(),
+					loader: 'css',
+				};
+			});
+		},
+	};
+};
 
 chokidar.watch('./src').on('all', () => {
-  esbuild.build(
-    {
-      define: { 'process.env.NODE_ENV': '"production"' },
-      loader: {
-        '.js': 'jsx',
-        '.ts': 'tsx',
-      },
-      minify: true,
-      logLevel: "info",
-      entryPoints: ["./src/index.tsx"],
-      bundle: true,
-      outfile: "public/main.js",
-      plugins: [sassPlugin()],
-      watch: true
-    }
-  ).then(() => {
-    serve.update();
-  }).catch(() => process.exit(1));
+	esbuild.build(
+		{
+			define: { 'process.env.NODE_ENV': '"production"' },
+			loader: {
+				'.js': 'jsx',
+				'.ts': 'tsx',
+			},
+			minify: true,
+			logLevel: 'info',
+			entryPoints: ['./src/index.tsx'],
+			bundle: true,
+			outfile: 'public/main.js',
+			plugins: [sassPlugin()],
+			watch: true
+		}
+	).then(() => {
+		serve.update();
+	// eslint-disable-next-line no-undef
+	}).catch(() => process.exit(1));
 
-  if (!SERVER_IS_RUNNING) {
-    serve.start({
-      root: ".",
-      live: true
-    });
+	if (!SERVER_IS_RUNNING) {
+		serve.start({
+			root: '.',
+			live: true
+		});
 
-    SERVER_IS_RUNNING = true;
-  }
+		SERVER_IS_RUNNING = true;
+	}
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import './style.scss'
-
-import { Button } from "../packages/Button/lib/button.js";
-
-const args = {
-  backgroundColor: null,
-  primary: true,
-  size: 'medium',
-  onClick: undefined,
-  label: 'Click me!!!'
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <Button {...args} />,
-  document.getElementById('root')
+	<h1>hola</h1>,
+	document.getElementById('root')
 );
